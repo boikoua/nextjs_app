2eb4c17f1e9b60565a6ff5249c006a37c88eefa3
Dmytro Boiko <iamboikodmytro@gmail.com>
2025-01-15 16:39:35 +0200

    start new project for learn next

--- components/shared/index.ts
+++
@@ -1,8 +0,0 @@
-// Общий файл для всех компонент в папке shared
-
-export { Container } from './container';
-export { Header } from './header';
-export { Title } from './title';
-export { Categories } from './categories';
-export { SortPopup } from './sort-popup';
-export { TopBar } from './top-bar';
