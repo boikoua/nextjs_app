463107a766ad513325d8399ee58493db2438114e
Dmytro Boiko <iamboikodmytro@gmail.com>
2025-01-15 17:47:14 +0200

    styles in Next JS

--- src/app/lesson.tsx
+++ src/app/lesson.tsx
@@ -1,22 +1,30 @@
+'use client';
 // Так как мы использовали хуки, то помечаем что это use client
 
-import React from 'react';
+import React, { useState } from 'react';
+import style from './lesson.module.css';
 
 type LessonProps = {
   title: string;
   shortSummary?: string;
 };
 
-// type LikeState = 'unset' | 'Liked' | 'dislike';
+type LikeState = 'unset' | 'Liked' | 'dislike';
 
 export const Lesson: React.FC<LessonProps> = ({ title, shortSummary }) => {
-  return (
-    <details>
-      <summary>
-        <h2>{title}</h2>
-      </summary>
+  const [like, setLike] = useState<LikeState>('unset');
 
+  const handleSetLike = () => setLike('Liked');
+
+  return (
+    <div>
+      <h2 className={style.header}>
+        {title}{' '}
+        <button type="button" onClick={handleSetLike}>
+          {like === 'unset' ? 'like' : like}
+        </button>
+      </h2>
       <p>{shortSummary}</p>
-    </details>
+    </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ export default async function Home() {
 
   return (
     <main className={styles.main}>
+      <h2 className="header">React</h2>
       <ul>
         {courses.lessons.map((lesson) => {
           return (
