5864f9b0c07a9a08d6f36e06d253ec478814a292
Dmytro Boiko <iamboikodmytro@gmail.com>
2025-01-16 20:04:39 +0200

    articles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,23 @@
+import Link from 'next/link';
 import { getAllArticles } from './(server)/api';
 import styles from './page.module.css';
+import { ROUTING } from './routing';
 
 export default async function Home() {
   const allArticles = await getAllArticles();
 
   const showArticles = allArticles.map((article) => (
-    <li key={article.name}>{article.header}</li>
+    <li key={article.name}>
+      <Link className={styles.link} href={ROUTING.article(article.name)}>
+        {article.header}
+      </Link>
+    </li>
   ));
 
   return (
     <>
       <h1>Усі статті мого блогу</h1>
-      <ul>{showArticles}</ul>
+      <ul className={styles.items}>{showArticles}</ul>
     </>
   );
 }
