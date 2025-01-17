9c60924fb65ffd2fe4816475a058a31b85d949bc
Dmytro Boiko <iamboikodmytro@gmail.com>
2025-01-17 14:16:18 +0200

    page for article

--- src/app/(server)/api.ts
+++ src/app/(server)/api.ts
@@ -4,3 +4,13 @@ import { delayed } from './delayed';
 export function getAllArticles() {
   return delayed(articles, { timeout: 1500, shouldFail: false });
 }
+
+export function getArticleByName(articleId: string) {
+  const article = articles.find((art) => art.name === articleId);
+
+  if (!article) {
+    throw new Error('This article not found');
+  }
+
+  return delayed(article);
+}
--- src/app/article/[articleId]/page.tsx
+++ src/app/article/[articleId]/page.tsx
@@ -1,7 +1,20 @@
-const ArticlePage = ({ params }: Params) => {
+import { getArticleByName } from '@/app/(server)/api';
+
+const ArticlePage = async ({ params }: Params) => {
   const { articleId } = params;
 
-  return <div>My article page ID: {articleId}</div>;
+  const article = await getArticleByName(articleId);
+
+  const { name, text, header, preview } = article;
+
+  return (
+    <article>
+      <h2>{header}</h2>
+      {text.map((line, index) => (
+        <p key={index}>{line}</p>
+      ))}
+    </article>
+  );
 };
 
 export default ArticlePage;
